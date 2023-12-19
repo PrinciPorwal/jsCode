19f1cd8e56aebea2e4b2b7f7eb962cf73203205f
Princi Porwal <133786604+PrinciPorwal@users.noreply.github.com>
2023-12-19 14:16:14 +0000

    push

---
+++ Basic/dataType2.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+//TOTAL DATATYPES
+// number => 2 to power 53
+// bigint->if number is too large we use it in trading & stock market
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined => 
+// symbol =>for uniqueness
+
+// object
+console.log(typeof "princi");//string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof symbol);//undefined
